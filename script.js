e66d971c98c50ecaddac24a43bf03cf770eb7a00
Galina Smirnova <gal4enok13@gmail.com>
2021-06-16 15:26:22 +0300

    date finish

--- script.js
+++ script.js
@@ -22,21 +22,28 @@ request.send();
 function getBlockInfo(data) {       
     data.articles.forEach(function(elem) {
         let div = document.createElement('div');        
-        let date = Date.parse(elem.publishedAt);
+        let date = new Date(Date.parse(elem.publishedAt));
+        let dateNews;
+            if ((date.getMonth(date) + 1) < 10) {
+                dateNews = date.getDate(date) + "." + "0" + (date.getMonth(date) + 1) + "." + date.getFullYear(date);
+            } else {
+                dateNews = date.getDate(date) + "." + (date.getMonth(date) + 1) + "." + date.getFullYear(date);
+        };        
         div.className = "news_block";        
-        div.innerHTML = `<img src="${elem.urlToImage}"><h2><a href="${elem.url}">${elem.title}</a></h2><p class="date">${date}</p><p>${elem.description}</p>`;
+        div.innerHTML = `<img src="${elem.urlToImage}"><h2><a href="${elem.url}">${elem.title}</a></h2><p class="date">${dateNews} | ${elem.author}</p><p>${elem.description}</p>`;
         news.appendChild(div);
     }); 
 }
 form.onsubmit = function (evt) {
         let div = document.createElement('div'),
-        titleNews = document.getElementById('title'),
-        linkNews = document.getElementById('link_news'),
-        imgNews = document.getElementById('link_img'),
-        authorNews = document.getElementById('author'),
-        descriptionNews = document.getElementById('description');
+            titleNews = document.getElementById('title'),
+            linkNews = document.getElementById('link_news'),
+            imgNews = document.getElementById('link_img'),
+            authorNews = document.getElementById('author'),
+            dateSetNews = document.getElementById('date'),
+            descriptionNews = document.getElementById('description');
         div.className = "news_block"; 
-        div.innerHTML = `<img src="${imgNews.value}"><h2><a href="${linkNews.value}">${titleNews.value}</a></h2><p>${descriptionNews.value}</p>`;
+        div.innerHTML = `<img src="${imgNews.value}"><h2><a href="${linkNews.value}">${titleNews.value}</a></h2><p>${dateSetNews.value} | ${authorNews.value}</p><p>${descriptionNews.value}</p>`;
         news.prepend(div);
         return false;    
     } 
