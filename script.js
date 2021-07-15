11fc2b9eb0c6b660ae76ba68f545efd32badd826
Galina Smirnova <gal4enok13@gmal.com>
2021-07-15 12:43:17 +0300

    form

--- script.js
+++ script.js
@@ -24,11 +24,11 @@ function getBlockInfo(data) {
         let div = document.createElement('div'),        
             date = new Date(Date.parse(elem.publishedAt)),
             dateNews;
-            if ((date.getMonth(date) + 1) < 10) {
+        if ((date.getMonth(date) + 1) < 10) {
                 dateNews = date.getDate(date) + "." + "0" + (date.getMonth(date) + 1) + "." + date.getFullYear(date);
             } else {
                 dateNews = date.getDate(date) + "." + (date.getMonth(date) + 1) + "." + date.getFullYear(date);
-        };        
+        }       
         div.className = "news_block";        
         div.innerHTML = `<img src="${elem.urlToImage}"><h2><a href="${elem.url}">${elem.title}</a></h2><p class="date">${dateNews} | ${elem.author}</p><p>${elem.description}</p>`;
         news.appendChild(div);
@@ -40,14 +40,10 @@ form.onsubmit = function (evt) {
             imgNews = document.getElementById('link_img'),
             authorNews = document.getElementById('author'),
             dateSetNews = document.getElementById('date'),
-            descriptionNews = document.getElementById('description');           
-        
-        if (titleNews.value != '') {  
-                let div = document.createElement('div');       
-                div.className = "news_block"; 
-                div.innerHTML = `<img src="${imgNews.value}"><h2><a href="${linkNews.value}">${titleNews.value}</a></h2><p>${dateSetNews.value} | ${authorNews.value}</p><p>${descriptionNews.value}</p>`;
-                news.prepend(div);                
-                return false;    
-        }        
-        
+            descriptionNews = document.getElementById('description'),
+            div = document.createElement('div');       
+        div.className = "news_block"; 
+        div.innerHTML = `<img src="${imgNews.value}"><h2><a href="${linkNews.value}">${titleNews.value}</a></h2><p>${dateSetNews.value} | ${authorNews.value}</p><p>${descriptionNews.value}</p>`;
+        news.prepend(div);                
+        return false;
     } 
